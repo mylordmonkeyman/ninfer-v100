ee9aab0d4bb00e9e5071f9a82d90fe239401d799
mylordmonkeyman <m.d.saunders@gmail.com>
2026-09-18 21:23:08 -0500

    artifact: add mapped tensor materialization support

--- src/artifact/binder.h
+++ src/artifact/binder.h
@@ -30,11 +30,11 @@ struct HostMaterialization {
     ObjectHandle object;
 };
 
-struct MaterializationPlan {
+struct MappedTensorMaterialization {\n    ObjectHandle object;\n};\n\nstruct MaterializationPlan {
     std::size_t object_count            = 0;
     std::uint64_t device_capacity_bytes = 0;
     std::vector<DeviceMaterialization> device_objects;
-    std::vector<HostMaterialization> host_objects;
+    std::vector<HostMaterialization> host_objects;\n    std::vector<MappedTensorMaterialization> mapped_tensor_objects;
 };
 
 class Binder {
@@ -49,7 +49,7 @@ public:
     const ObjectDescriptor& descriptor(ObjectHandle handle) const;
     PayloadSpan payload(ObjectHandle handle) const;
     void materialize_on_device(ObjectHandle handle);
-    void retain_on_host(ObjectHandle handle);
+    void retain_on_host(ObjectHandle handle);\n    void retain_mapped_tensor(ObjectHandle handle);
     void validate_only(ObjectHandle handle);
     MaterializationPlan finish();
 
