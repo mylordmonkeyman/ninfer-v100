53b342efce00b4b5157dcd0ea6d2c2ccc700bc67
mylordmonkeyman <m.d.saunders@gmail.com>
2026-09-18 22:00:52 -0500

    volta: add runtime expert staging to MoE interface

--- src/targets/qwen3_8_flash_next/impl/moe.h
+++ src/targets/qwen3_8_flash_next/impl/moe.h
@@ -14,7 +14,8 @@ namespace ninfer::targets::qwen3_8_flash_next::detail {
                                                                   std::int32_t max_tokens);
 
 void flash_next_moe(const Tensor& input, const MoeWeights& weights, Tensor& output,
-                    WorkspaceArena& workspace, cudaStream_t stream);
+                    WorkspaceArena& workspace, cudaStream_t stream,
+                    void* expert_staging = nullptr, std::size_t expert_staging_bytes = 0);
 
 void flash_next_moe_bf16(const Tensor& input, const MoeBf16Weights& weights, Tensor& output,
                          WorkspaceArena& workspace, cudaStream_t stream);
