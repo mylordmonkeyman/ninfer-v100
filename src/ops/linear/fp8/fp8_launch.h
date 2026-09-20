2bb60302821a04a759daa552359ae208ca6fc64d
mylordmonkeyman <m.d.saunders@gmail.com>
2026-09-19 23:32:19 -0500

    feat(volta): expose FP8 QPN launch contract

--- src/ops/linear/fp8/fp8_launch.h
+++ src/ops/linear/fp8/fp8_launch.h
@@ -4,6 +4,8 @@
 
 #include <cuda_runtime.h>
 
+#include <cstdint>
+
 namespace ninfer::ops::detail {
 
 void launch_fp8_decode(const Tensor& x, const Weight& weight, Tensor& out, cudaStream_t stream);
@@ -12,5 +14,13 @@ void launch_fp8_vocabulary_a16_small_t(const Tensor& x, const Weight& weight, Te
                                        cudaStream_t stream);
 void launch_fp8_vocabulary_a16_gemm(const Tensor& x, const Weight& weight, Tensor& out,
                                     cudaStream_t stream);
+
+#ifdef NINFER_VOLTA_BUILD
+inline constexpr std::int32_t kFp8VoltaQpnRowsPerTile = 8;
+inline constexpr std::int32_t kFp8VoltaQpnMaxTokens   = 32;
+void launch_fp8_volta_qpn(const Tensor& x, const Weight& weight, Tensor& out, cudaStream_t stream);
+[[nodiscard]] bool fp8_volta_qpn_supported(std::int32_t n, std::int32_t k,
+                                           std::int32_t t) noexcept;
+#endif
 
 } // namespace ninfer::ops::detail
