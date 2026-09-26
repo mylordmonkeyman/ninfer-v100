24e3682e47f299cb2eab56bda6158b6f6e7ebdcb
mylordmonkeyman <m.d.saunders@gmail.com>
2026-09-25 21:22:40 -0500

    Trace raw attention preparation in chunked prefill as well as decode

--- src/targets/qwen3_8_flash_next/impl/text_decode.cpp
+++ src/targets/qwen3_8_flash_next/impl/text_decode.cpp
@@ -756,6 +756,11 @@ void flash_next_text_prefill_chunk(const TextModelView& model, const Tensor& emb
         flash_next_hyper_prepare(round_ws.hyper_hidden, model.layers[layer].attention_hyper,
                                  round_ws.hyper_scratch, round_ws.block_input, stream);
         stage_ledger_record(stream, FlashNextStageId::Hyper_PrepareAttn);
+#if defined(NINFER_VOLTA_BUILD)
+        if (layer == 1 && std::getenv("NINFER_PHASE11_TRACE_ATTENTION_PREPARE_RAW") != nullptr) {
+            emit_state(prefix + "attn_block_input_fp32", round_ws.hyper_scratch.mixed_fp32);
+        }
+#endif
         emit_state(prefix + "attn_block_input", round_ws.block_input);
 
         // Execute QSA or GDN attention
